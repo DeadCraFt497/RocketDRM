8ed88422aaf1d76ca9042d34cfb56c78fe4d56cb
DeadCraFt4967 <deadcraft_3@proton.me>
2025-05-18 08:20:35 -0500

    Update Utils.java

--- abyss-encryption-1.4.1/src/Utils.java
+++ abyss-encryption-1.4.1/src/Utils.java
@@ -10,16 +10,12 @@ public class Utils
         {
             IvParameterSpec iv = new IvParameterSpec( parseHexBinary( "320184ae2fe5a740722a420d94bb1e21" ) );
             SecretKeySpec key = new SecretKeySpec( parseHexBinary( "6b61499284acab1ef47fca087eff654e" ), "AES" );
-
             Cipher cipher = Cipher.getInstance( "AES/CBC/PKCS5PADDING" );
             cipher.init( Cipher.ENCRYPT_MODE, key, iv );
-
             byte[ ] bytes = cipher.doFinal( in.getBytes( ) );
-
             StringBuilder builder = new StringBuilder( );
             for( int i = 0; i < bytes.length; i++ )
                 builder.append( String.format( "%02x", bytes[ i ] ) );
-
             return builder.toString( );
         }
         catch( Exception e )
